b4926639a6d6ad6c0fcfd6101cb1c79b346b1e81
samiulalimsaad <saadnbiuraj@gmail.com>
2022-04-16 23:24:48 +0600

    configured react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import About from "./Components/About";
+import Blogs from "./Components/Blogs";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+import SignUp from "./Components/SignUp";
+import Footer from "./Footer";
+
 function App() {
     return (
         <div>
-            <h1 class="text-3xl font-bold underline">Hello world!</h1>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/blogs" element={<Blogs />} />
+            </Routes>
+            <Footer />
         </div>
     );
 }
---
+++ src/Components/Blogs/Blog/Q1.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Q1 = () => {
+    return <div>Q1 Page</div>;
+};
+
+export default Q1;
