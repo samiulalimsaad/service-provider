ddc852c349fc28242d9d09cdc77d7c3a35da37d2
samiulalimsaad <saadnbiuraj@gmail.com>
2022-04-18 03:33:32 +0600

    solved private route issue

--- src/Components/utilities/PrivateRoute.jsx
+++ src/Components/utilities/PrivateRoute.jsx
@@ -1,21 +1,13 @@
 import { useAuthState } from "react-firebase-hooks/auth";
-import { Navigate, useNavigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { auth } from "../Firebase.init";
 
 const PrivateRoute = ({ children }) => {
-    const navigate = useNavigate();
+    const location = useLocation();
     const [user, loading] = useAuthState(auth);
 
     if (!loading && !user) {
-        // navigate("/login", {
-        //     state: {
-        //         replace: true,
-        //         from: location,
-        //     },
-        // });
-        // return "null";
-        return <Navigate to="/login" replace />;
-        // return <Navigate to="/login" state={{ from: location }} replace />;
+        return <Navigate to="/login" state={{ from: location }} replace />;
     }
 
     return children;
